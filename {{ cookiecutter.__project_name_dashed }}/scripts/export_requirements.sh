cdaa24e15b0acf6ec8fb09e374fe749c3a8d84ba
William <william@fayers.com>
2024-09-29 19:21:42 +0100

    feat: update scripts

--- {{ cookiecutter.__project_name_dashed }}/scripts/export_requirements.sh
+++ {{ cookiecutter.__project_name_dashed }}/scripts/export_requirements.sh
@@ -1,36 +1,56 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Updated from https://github.com/alexfayers/new_python_project/blob/main/scripts/export_requirements.sh
+# This script takes care of exporting the project requirements to requirements.txt files
+# for different groups defined in the pyproject.toml file.
 
 # Ensure the poetry lock file is up to date
-echo "[-] Making sure lock file is up to date..."
+echo "Making sure lock file is up to date..."
+if ! poetry lock; then
+    echo "Failed to update the lock file. Aborting."
+    exit 3
+fi
 
-poetry lock
-git add poetry.lock
-git commit -m "Update poetry.lock"
+# Check if there are changes to commit
+if [[ -n $(git status --porcelain) ]]; then
+    echo "Committing changes to the lock file..."
+    git add poetry.lock
+    git commit -m "chore: update lock file"
+else
+    echo "No changes to commit for the lock file."
+fi
 
 # Export the base project requirements poetry into requirements.txt format.
-echo "[-] Exporting main requirements..."
-poetry export --format=requirements.txt \
+echo "Exporting main requirements..."
+if ! poetry export --format=requirements.txt \
     --only="main" \
     --without-hashes \
-    --output="requirements.txt"
+    --output="requirements.txt"; then
+    echo "Failed to export main requirements. Aborting."
+    exit 4
+fi
 
 # Define the extra group dependencies
 declare -a arr=("dev" "types" "test" "docs" "release")
 
 for group in "${arr[@]}"
 do
-    echo "[-] Exporting $group requirements..."
-    poetry export --format=requirements.txt \
+    echo "Exporting $group requirements..."
+    if ! poetry export --format=requirements.txt \
         --without-hashes \
         --only="$group" \
-        --output="requirements-$group.txt"
+        --output="requirements-$group.txt"; then
+        echo "Failed to export $group requirements. Aborting."
+        exit 4
+    fi
 done
 
-echo "[+] All requirements exported."
+echo "All requirements exported."
 
-git add "requirements*.txt"
-git commit -m "Update requirements files"
+if [[ -n $(git status --porcelain) ]]; then
+    git add "requirements*.txt"
+    git commit -m "chore: update requirement files"
+else
+    echo "No changes to commit for requirements files."
+fi
 
-exit 0
+return 0  # Success but don't exit the script, as it is sourced by another script
--- {{ cookiecutter.__project_name_dashed }}/scripts/release.sh
+++ {{ cookiecutter.__project_name_dashed }}/scripts/release.sh
@@ -1,18 +1,26 @@
 #!/bin/bash -e
 
 # This script takes care of:
-# 1. Updating the version number in the pyproject.toml file
-# 2. Generating a new changelog based on the commit history
-# 3. Committing the changes to the git repository
-# 4. Creating a new tag for the release
-# 5. Building the package
-# 6. Publishing the package to PyPI
+# 1. Updating the version numbers across the project
+# 2. Update requirements and their files across the project
+# 3. Generating a new changelog based on the commit history
+# 4. Committing the changes to the git repository
+# 5. Creating a new tag for the release
+# 6. Building the package
+# 7. Publishing the package to PyPI
 
 # The script takes the type of version to release ('major', 'minor', or 'patch') as an argument.
 # It is incompatible with pre-release versions for simplicity.
 
 # This script requires both Poetry and git to be installed and configured on the system,
 # including authentication for pushing to the remote repository and publishing to PyPI.
+
+# Error codes:
+# 1 - Uncommitted changes in the git repository
+# 2 - Invalid version type
+# 3 - Failed to update the lock file
+# 4 - Failed to export requirements
+# 5 - Failed to pull the latest changes from the remote repository
 
 # Check if the git repo has any uncommitted changes
 if [[ -n $(git status --porcelain) ]]; then
@@ -23,28 +31,33 @@ fi
 # Check if an argument was passed
 if [ -z "$1" ]; then
     echo "No version type supplied. Please specify 'major', 'minor', or 'patch'."
-    exit 1
+    exit 2
 fi
 
 # Validate the argument as a version type
 if [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" ]]; then
     echo "Invalid version type. Please specify 'major', 'minor', or 'patch'."
-    exit 1
+    exit 2
 fi
 
 # Get the current script's directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Ensure requirements.txt files are up to date
-"$SCRIPT_DIR/export_requirements.sh"
+# Update requirements and their files
+echo "Updating requirements and their files..."
+poetry update
+source "$SCRIPT_DIR/export_requirements.sh"
+echo "Requirements and their files updated successfully."
 
 # Clear the current changelog - it gets regenerated fully on each release
 echo '' > CHANGELOG.md
 
 # Update the changelog
+echo "Generating a new changelog..."
 gitchangelog > CHANGELOG.md
 git add CHANGELOG.md
-git commit -m "Update CHANGELOG.md"
+git commit -m "chore: update changelog"
+echo "Changelog generated successfully."
 
 # Get the current version number
 current_version=$(poetry version -s)
@@ -56,19 +69,37 @@ poetry version "$1"
 new_version=$(poetry version -s)
 
 # Update __init__.py version numbers
+echo "Updating __init__.py version numbers to $new_version..."
 find source -type f -name "__init__.py" -exec sed -i "s/__version__ = \"$current_version\"/__version__ = \"$new_version\"/g" {} \;
+echo "__init__.py version numbers updated successfully."
 
 # Update the version number in the git repo
+echo "Committing version bump to the git repository..."
 git add .
-git commit -m "Bump version: $current_version -> $new_version"
+git commit -m "chore: bump version from $current_version to $new_version"
+echo "Version bump committed successfully."
+
+# Pull the latest changes from the remote repository
+git config pull.rebase true  # Ensure that the pull is a rebase
+echo "Pulling the latest changes from the remote repository..."
+if ! git pull origin main; then
+    echo "Failed to pull the latest changes from the remote repository."
+    exit 5
+fi
 
 # Create a new tag and push it to the remote repository
+echo "Creating a new tag for the release..."
 git tag -a "v$new_version" -m "Release $new_version"
-
+echo "Pushing the new tag to the remote repository..."
 git push --follow-tags
+echo "Release $new_version created successfully."
 
 # Build the package for PyPI
+echo "Building the package..."
 poetry build
+echo "Package built successfully."
 
 # Publish the package to PyPI
+echo "Publishing the package to PyPI..."
 poetry publish
+echo "Package published to PyPI successfully."
